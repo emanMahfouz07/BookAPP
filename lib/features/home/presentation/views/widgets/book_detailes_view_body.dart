99a0911193b4cfbfc3b3d910e4dd8e1a68420fb3
Eman Mahfouz <emankahled614@gmail.com>
2024-03-23 21:06:15 +0200

    Add Custom book Item to book detailes

--- lib/features/home/presentation/views/widgets/book_detailes_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_detailes_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
+import 'package:bookly/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -6,11 +7,18 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30.0),
-      child: const Column(children: [
-        CustomBookDetailsAppBar(),
-      ],),
+      child: Column(
+        children: [
+          const CustomBookDetailsAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * .17),
+            child: const CustomBookImage(),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/features/home/presentation/views/widgets/custom_book_image.dart
+++ lib/features/home/presentation/views/widgets/custom_book_image.dart
@@ -1,13 +1,13 @@
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class CustomBookImage extends StatelessWidget {
+  const CustomBookImage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
-      aspectRatio: 1,
+      aspectRatio: 2.7 / 4,
       child: Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16),
