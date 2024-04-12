8891ba7dc78cf2973e930a20d3bc31fefc94f487
Eman Mahfouz <emankahled614@gmail.com>
2024-04-12 15:36:49 +0200

    add home repo

--- lib/features/splash/presentation/views/widgets/sliding_text.dart
+++ lib/features/splash/presentation/views/widgets/sliding_text.dart
@@ -11,16 +11,15 @@ class SlidingText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-      animation: slidingAnimeation,
-      builder: (context,_) {
-        return SlideTransition(
-          position: slidingAnimeation,
-          child: const Text(
-            'Read Free Books',
-            textAlign: TextAlign.center,
-          ),
-        );
-      }
-    );
+        animation: slidingAnimeation,
+        builder: (context, _) {
+          return SlideTransition(
+            position: slidingAnimeation,
+            child: const Text(
+              'Read Free Books',
+              textAlign: TextAlign.center,
+            ),
+          );
+        });
   }
 }
