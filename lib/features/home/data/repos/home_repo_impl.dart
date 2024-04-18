04a75464c25ebfb751535d7e3a92ad169a894000
Eman Mahfouz <emankahled614@gmail.com>
2024-04-18 14:50:42 +0200

    create featured books state

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -9,7 +9,7 @@ class HomeRepoImple implements HomeRepo {
   final ApiServices apiServices;
   HomeRepoImple(this.apiServices);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() async {
+  Future<Either<Failure, List<BookModel>>> fetchNewsetBooks() async {
     try {
       var data =
           await apiServices.get(endPoint: '/volumes?q=subject:programming');
@@ -27,8 +27,20 @@ class HomeRepoImple implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data =
+          await apiServices.get(endPoint: '/volumes?q=subject:programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
   }
 }
