f823f32e125e0aaef4255d550462d1f7dab65838
Eman Mahfouz <emankahled614@gmail.com>
2024-04-25 15:12:15 +0200

    setup service locator

--- lib/features/home/presentation/manger/Newset Books Cubit/newset_books_cubit_cubit.dart
+++ lib/features/home/presentation/manger/Newset Books Cubit/newset_books_cubit_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly/core/Errors/failure.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'newset_books_cubit_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewsetBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchNewsetBooks();
+    result.fold((failure) => emit(NewsetBooksFailure(failure.errMessage)),
+        (books) => emit(NewsetBooksSuccess(books)));
+  }
 }
